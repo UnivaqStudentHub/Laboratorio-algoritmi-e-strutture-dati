fa27c2ab5d88f5f3aae1a64df2512e283dad8f4d
Giacomix02 <paoloccigiacomo@gmail.com>
2022-11-15 14:45:02 +0100

    esercizio

---
+++ Primo parziale/Compito 2021/src/Chiamata.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Chiamata implements Comparable<Chiamata> {
+    private double minutiDurata; // durata della telefonata in minuti
+    private String numTel; // numero di telefono chiamato
+    private Date InizioDataOra; // timestamp di inizio chiamata
+
+    public Chiamata(String numTel, Date InizioDataOra, double minutiDurata) {
+        this.minutiDurata = minutiDurata;
+        this.numTel = numTel;
+        this.InizioDataOra = InizioDataOra;
+    }
+
+    public double getDurata() {
+        return minutiDurata;
+    }
+
+    public String getNumTel() {
+        return numTel;
+    }
+
+    public Date getDataOra() {
+        return InizioDataOra;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj == null) || !(obj instanceof Chiamata)) return false;
+        Chiamata c2 = (Chiamata) obj;
+        return (minutiDurata == c2.minutiDurata &&
+                numTel.equals(c2.numTel) &&
+                InizioDataOra.equals(c2.InizioDataOra));
+    }
+
+    public String toString() {
+        return numTel + ", " + InizioDataOra + ", " + minutiDurata;
+    }
+
+    public int compareTo(Chiamata c) {
+        return InizioDataOra.compareTo(c.InizioDataOra);
+    }
+} //fine-classe
